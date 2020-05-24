5cf33c17343780a0a0eda222b81e60df24ab5e81
James Cole <james@firefly-iii.org>
2020-05-24 17:46:36 +0200

    Better validation

--- app/Http/Controllers/TelemetryController.php
+++ app/Http/Controllers/TelemetryController.php
@@ -26,6 +26,7 @@ class TelemetryController extends Controller
      *
      * @param Request $request
      *
+     * @throws \JsonException
      * @return JsonResponse
      */
     public function submit(Request $request): JsonResponse
@@ -48,8 +49,15 @@ class TelemetryController extends Controller
          * Take the body, if its body is valid JSON. Assumes header is Content-Type = application/json
          */
         $body = $request->all();
-        if ([] === $body) {
+        if ([] === $body || !is_array($body)) {
             Log::error(sprintf('%s: Body is empty array or not content-type header.', $uuid));
+
+            return response()->json(['uuid' => $uuid], 400);
+        }
+
+        // validate body content:
+        if (false === $this->validateBody($body)) {
+            Log::error('validateBody says false.');
 
             return response()->json(['uuid' => $uuid], 400);
         }
@@ -64,6 +72,7 @@ class TelemetryController extends Controller
             $submission->save();
         } catch (QueryException $e) {
             Log::error(sprintf('%s: Query exception: %s', $uuid, $e->getMessage()));
+            Log::error(sprintf('%s: Body:', $uuid), $body);
 
             return response()->json(['uuid' => $uuid], 500);
         }
@@ -81,5 +90,33 @@ class TelemetryController extends Controller
         $agents = config('telemetry.allowed_ua');
 
         return Str::contains($userAgent, $agents);
+    }
+
+    /**
+     * Pretty basic check. Each entry should have some keys.
+     *
+     * @param array $body
+     */
+    private function validateBody(array $body): bool
+    {
+        $fields = ['installation_id', 'collected_at', 'type', 'key', 'value'];
+        $looped = false;
+        foreach ($body as $index => $row) {
+            $looped = true;
+            foreach ($fields as $field) {
+                if (!isset($row[$field])) {
+                    Log::error(sprintf('Body contains no field for "%s" in row %d.', $field, $index), $body);
+
+                    return false;
+                }
+            }
+        }
+        if (false === $looped) {
+            Log::error('No entries in submission.');
+
+            return false;
+        }
+
+        return true;
     }
 }
